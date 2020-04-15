fa99ca27d606b6aeaf99e2482d2d56af6bb95454
Verrelio C. Rizky <verrelioragna@gmail.com>
2020-04-15 15:07:32 +0700

    create base file for list genre

---
+++ Movie List/Features/List Genres/ListGenresViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ListGenresViewController.swift
+//  Movie List
+//
+//  Created by Verrelio C. Rizky on 15/04/20.
+//  Copyright © 2020 Verrelio C. Rizky. All rights reserved.
+//
+
+import UIKit
+
+class ListGenresViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
