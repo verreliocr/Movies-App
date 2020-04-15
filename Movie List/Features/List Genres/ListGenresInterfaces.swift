46178701ff05899f86a0bd3ad74ed8a15ae2a793
Verrelio C. Rizky <verrelioragna@gmail.com>
2020-04-15 15:27:41 +0700

    setup interfaces for list genre

--- Movie List/Features/List Genres/ListGenresInterfaces.swift
+++ Movie List/Features/List Genres/ListGenresInterfaces.swift
@@ -7,3 +7,27 @@
 //
 
 import Foundation
+import UIKit
+
+protocol IListGenresView: class {
+    func reloadView()
+    func handleError(type: ErrorType, retryAction: @escaping (() -> Void))
+    func showLoading(_ show: Bool)
+}
+
+protocol IListGenresInteractor {
+    func getListGenres(completion: @escaping (([GenreModelItem]?, ErrorType?) -> Void))
+}
+
+protocol IListGenresPresenter {
+    func setView(_ view: IListGenresView)
+    func viewWillAppear()
+    func getNumberOfRows() -> Int
+    func getGenreName(at index: Int) -> String
+    func getBackColor(at index: Int) -> UIColor
+    func didSelectGenre(at index: Int)
+}
+
+protocol IListGenresWireframe {
+    func navigateToListMovie(with id: Int)
+}
